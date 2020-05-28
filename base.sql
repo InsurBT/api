1dc53aa0fec0e664f7b479c4020b0aff17330f76
tarekrahale <tarekrahale@gmail.com>
2020-05-28 22:58:43 +0000

    ajout du script de creation des tables de la bdd

---
+++ base.sql
@@ -0,0 +1,25 @@
+CREATE TABLE agence (
+    code NUMBER NOT NULL GENERATED ALWAYS AS IDENTITY,
+    label VARCHAR(50),
+    CONSTRAINT agence_pk PRIMARY KEY (code)
+);
+
+CREATE TABLE utilisateur (
+    cod NUMBER NOT NULL GENERATED ALWAYS AS IDENTITY,
+    nom VARCHAR(20) NOT NULL,
+    nomComplet VARCHAR(50),
+    code_agence NOT NULL NUMBER,
+    CONSTRAINT nom_unique UNIQUE (nom),
+    CONSTRAINT utilisateur_pk PRIMARY KEY (cod),
+    CONSTRAINT agence_fk FOREIGN KEY (code_agence) REFERENCES agence(code)
+);
+
+CREATE TABLE prestation (
+    id NUMBER NOT NULL GENERATED ALWAYS AS IDENTITY,
+    type VARCHAR(50) NOT NULL,
+    nbrActes NUMBER NOT NULL,
+    montantPaye NUMBER NOT NULL,
+    montantEngage NUMBER NOT NULL,
+    CONSTRAINT prestation_pk PRIMARY KEY (id)
+)
+
